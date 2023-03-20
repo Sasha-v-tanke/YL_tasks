2a6362e29bf57185ac79d7ceadf8bc476b0fe467
Sasha-v-tanke <s4sha-v-tanke@yandex.ru>
2023-03-20 18:36:03 +0300

    Варианты выбора

--- flask/script1.py
+++ flask/script1.py
@@ -3,28 +3,9 @@ from flask import Flask, render_template, request
 app = Flask(__name__)
 
 
-@app.route('/', methods=['GET'])
-def index():
-    return render_template('index.html')
-
-
-@app.route('/', methods=['POST'])
-def submit_form():
-    # Получение данных из формы
-    surname = request.form['surname']
-    name = request.form['name']
-    email = request.form['email']
-    jobs = []
-    for i in range(1, 17):
-        jobs.append(request.form.get("job" + str(i)))
-    education = request.form['qualification']
-    gender = request.form['gender']
-    motivation = request.form['motivation']
-    stay_on_mars = request.form.get('stay_on_mars')
-
-    file = request.files['file']
-
-    return 'Анкета успешно отправлена!'
+@app.route('/choice/<planet_name>')
+def planet_invite(planet_name):
+    return render_template('index.html', name=planet_name)
 
 
 if __name__ == '__main__':
