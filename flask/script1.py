51c042a666431107fb22ad9d33399551e201c19d
Sasha-v-tanke <s4sha-v-tanke@yandex.ru>
2023-03-27 17:46:10 +0300

    Тренировки в полёте

--- flask/script1.py
+++ flask/script1.py
@@ -10,5 +10,10 @@ def index():
     return render_template("base.html", title=title)
 
 
+@app.route('/training/<profession>')
+def training(profession):
+    return render_template('training.html', profession=profession.lower())
+
+
 if __name__ == "__main__":
     app.run()
