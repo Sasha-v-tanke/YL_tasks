13b725bf9fb324d2e55588fe36185a07092dd3cb
Sasha-v-tanke <s4sha-v-tanke@yandex.ru>
2023-03-27 17:58:32 +0300

    Список профессий

--- flask/script1.py
+++ flask/script1.py
@@ -1,4 +1,5 @@
 from flask import Flask, render_template, request
+from setuptools._entry_points import render
 
 app = Flask(__name__)
 
@@ -13,6 +14,13 @@ def index():
 @app.route('/training/<profession>')
 def training(profession):
     return render_template('training.html', profession=profession.lower())
+
+
+@app.route('/list_prof/<list>')
+def list_prof(list):
+    professions = ['Пилот', 'Инженер', 'Астроном', 'Биолог', 'Химик', 'Геолог', 'Врач', 'Строитель']
+
+    return render_template('list_prof.html', professions=professions, list=list)
 
 
 if __name__ == "__main__":
