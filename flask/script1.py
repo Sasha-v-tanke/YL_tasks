9ac434298e74258a8ab90011cde3a6341fd7d719
Sasha-v-tanke <s4sha-v-tanke@yandex.ru>
2023-03-27 16:54:39 +0300

    Результат отбора

--- flask/script1.py
+++ flask/script1.py
@@ -1,11 +1,10 @@
-from flask import Flask, render_template, request
-
+from flask import Flask, render_template
 app = Flask(__name__)
 
 
-@app.route('/choice/<planet_name>')
-def planet_invite(planet_name):
-    return render_template('index.html', name=planet_name)
+@app.route('/results/<string:nickname>/<int:level>/<float:rating>')
+def show_results(nickname, level, rating):
+    return render_template('index.html', level=level, nickname=nickname, rating=rating)
 
 
 if __name__ == '__main__':
