26b4d31bba73994c3e190295b8e537e31f3d1452
Sasha-v-tanke <s4sha-v-tanke@yandex.ru>
2023-03-27 17:35:11 +0300

    Готовимся к миссии

--- flask/script1.py
+++ flask/script1.py
@@ -1,33 +1,14 @@
-from flask import Flask, render_template, request, redirect, url_for
-import os
+from flask import Flask, render_template, request
 
 app = Flask(__name__)
 
-# Установка директории для загрузки фотографий
-UPLOAD_FOLDER = 'static/img'
-app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
 
-
-@app.route('/')
+@app.route("/")
+@app.route("/index")
 def index():
-    # Отображение страницы с формой
-    return render_template('index.html')
+    title = request.args.get("title", "Колонизация Марса")
+    return render_template("base.html", title=title)
 
 
-@app.route('/', methods=['POST'])
-def upload_image():
-    # Загрузка и сохранение фотографии
-    file = request.files['image']
-    filename = file.filename
-    file.save(os.path.join(app.config['UPLOAD_FOLDER'], filename))
-    return redirect(url_for('show_image', filename=filename))
-
-
-@app.route('/<filename>')
-def show_image(filename):
-    # Отображение загруженной фотографии на странице
-    return render_template('show_image.html', filename=filename)
-
-
-if __name__ == '__main__':
-    app.run(debug=True)
+if __name__ == "__main__":
+    app.run()
