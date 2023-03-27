832beb5a4754b906e4c7ef12f0cadc089cbb02a3
Sasha-v-tanke <s4sha-v-tanke@yandex.ru>
2023-03-27 17:03:17 +0300

    Загрузка файла

--- flask/script1.py
+++ flask/script1.py
@@ -1,10 +1,32 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request, redirect, url_for
+import os
+
 app = Flask(__name__)
 
+# Установка директории для загрузки фотографий
+UPLOAD_FOLDER = 'static/img'
+app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
 
-@app.route('/results/<string:nickname>/<int:level>/<float:rating>')
-def show_results(nickname, level, rating):
-    return render_template('index.html', level=level, nickname=nickname, rating=rating)
+
+@app.route('/')
+def index():
+    # Отображение страницы с формой
+    return render_template('index.html')
+
+
+@app.route('/', methods=['POST'])
+def upload_image():
+    # Загрузка и сохранение фотографии
+    file = request.files['image']
+    filename = file.filename
+    file.save(os.path.join(app.config['UPLOAD_FOLDER'], filename))
+    return redirect(url_for('show_image', filename=filename))
+
+
+@app.route('/<filename>')
+def show_image(filename):
+    # Отображение загруженной фотографии на странице
+    return render_template('show_image.html', filename=filename)
 
 
 if __name__ == '__main__':
